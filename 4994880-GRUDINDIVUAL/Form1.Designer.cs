3bfe2cd891e82beb1b02fc6d4e305cf391c207df
unknown <fabose.herampos.617@gmail.com>
2025-03-04 21:16:47 -0600

    First Commit

---
+++ 4994880-GRUDINDIVUAL/Form1.Designer.cs
@@ -0,0 +1,394 @@
+﻿namespace _4994880_GRUDINDIVUAL
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtEmpleado = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.txtDiaseis = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.txtDiacinco = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.txtDiacuatro = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.txtDiatres = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtDiados = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtDiauno = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.txtTotal = new System.Windows.Forms.TextBox();
+			this.txtSalario = new System.Windows.Forms.TextBox();
+			this.btnConsulta = new System.Windows.Forms.Button();
+			this.btnIngreso = new System.Windows.Forms.Button();
+			this.btnModificacion = new System.Windows.Forms.Button();
+			this.btnEliminacion = new System.Windows.Forms.Button();
+			this.btnCancelar = new System.Windows.Forms.Button();
+			this.btnAceptar = new System.Windows.Forms.Button();
+			this.btnCalcular = new System.Windows.Forms.Button();
+			this.btnRegistro = new System.Windows.Forms.Button();
+			this.btnSalir = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(260, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(189, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "EMPLEADOS - SALARIO SEMANALA";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 55);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(60, 13);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Empleado :";
+			// 
+			// txtEmpleado
+			// 
+			this.txtEmpleado.Location = new System.Drawing.Point(59, 86);
+			this.txtEmpleado.Name = "txtEmpleado";
+			this.txtEmpleado.Size = new System.Drawing.Size(316, 20);
+			this.txtEmpleado.TabIndex = 2;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.groupBox1.Controls.Add(this.txtDiaseis);
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.txtDiacinco);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.txtDiacuatro);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Controls.Add(this.txtDiatres);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.txtDiados);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.txtDiauno);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Location = new System.Drawing.Point(59, 138);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(523, 111);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Horas Laboradas";
+			// 
+			// txtDiaseis
+			// 
+			this.txtDiaseis.Location = new System.Drawing.Point(447, 65);
+			this.txtDiaseis.Name = "txtDiaseis";
+			this.txtDiaseis.Size = new System.Drawing.Size(32, 20);
+			this.txtDiaseis.TabIndex = 11;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(444, 33);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(32, 13);
+			this.label7.TabIndex = 10;
+			this.label7.Text = "Dia 6";
+			// 
+			// txtDiacinco
+			// 
+			this.txtDiacinco.Location = new System.Drawing.Point(371, 65);
+			this.txtDiacinco.Name = "txtDiacinco";
+			this.txtDiacinco.Size = new System.Drawing.Size(32, 20);
+			this.txtDiacinco.TabIndex = 9;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(368, 33);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(32, 13);
+			this.label8.TabIndex = 8;
+			this.label8.Text = "Dia 5";
+			// 
+			// txtDiacuatro
+			// 
+			this.txtDiacuatro.Location = new System.Drawing.Point(295, 65);
+			this.txtDiacuatro.Name = "txtDiacuatro";
+			this.txtDiacuatro.Size = new System.Drawing.Size(32, 20);
+			this.txtDiacuatro.TabIndex = 7;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(292, 33);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(32, 13);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "Dia 4";
+			// 
+			// txtDiatres
+			// 
+			this.txtDiatres.Location = new System.Drawing.Point(215, 65);
+			this.txtDiatres.Name = "txtDiatres";
+			this.txtDiatres.Size = new System.Drawing.Size(32, 20);
+			this.txtDiatres.TabIndex = 5;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(212, 33);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(32, 13);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Dia 3";
+			// 
+			// txtDiados
+			// 
+			this.txtDiados.Location = new System.Drawing.Point(133, 65);
+			this.txtDiados.Name = "txtDiados";
+			this.txtDiados.Size = new System.Drawing.Size(32, 20);
+			this.txtDiados.TabIndex = 3;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(130, 31);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(32, 13);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "Dia 2";
+			// 
+			// txtDiauno
+			// 
+			this.txtDiauno.Location = new System.Drawing.Point(48, 63);
+			this.txtDiauno.Name = "txtDiauno";
+			this.txtDiauno.Size = new System.Drawing.Size(32, 20);
+			this.txtDiauno.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(45, 31);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(32, 13);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Dia 1";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(114, 265);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(122, 13);
+			this.label9.TabIndex = 4;
+			this.label9.Text = "Horas Totales laboradas";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(430, 265);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(79, 13);
+			this.label10.TabIndex = 5;
+			this.label10.Text = "Salario a Pagar";
+			// 
+			// txtTotal
+			// 
+			this.txtTotal.Location = new System.Drawing.Point(136, 302);
+			this.txtTotal.Name = "txtTotal";
+			this.txtTotal.Size = new System.Drawing.Size(100, 20);
+			this.txtTotal.TabIndex = 6;
+			// 
+			// txtSalario
+			// 
+			this.txtSalario.Location = new System.Drawing.Point(409, 302);
+			this.txtSalario.Name = "txtSalario";
+			this.txtSalario.Size = new System.Drawing.Size(100, 20);
+			this.txtSalario.TabIndex = 7;
+			// 
+			// btnConsulta
+			// 
+			this.btnConsulta.Location = new System.Drawing.Point(107, 345);
+			this.btnConsulta.Name = "btnConsulta";
+			this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+			this.btnConsulta.TabIndex = 8;
+			this.btnConsulta.Text = "Consulta";
+			this.btnConsulta.UseVisualStyleBackColor = true;
+			this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+			// 
+			// btnIngreso
+			// 
+			this.btnIngreso.Location = new System.Drawing.Point(218, 345);
+			this.btnIngreso.Name = "btnIngreso";
+			this.btnIngreso.Size = new System.Drawing.Size(75, 23);
+			this.btnIngreso.TabIndex = 9;
+			this.btnIngreso.Text = "Ingreso";
+			this.btnIngreso.UseVisualStyleBackColor = true;
+			this.btnIngreso.Click += new System.EventHandler(this.btnIngreso_Click);
+			// 
+			// btnModificacion
+			// 
+			this.btnModificacion.Location = new System.Drawing.Point(442, 345);
+			this.btnModificacion.Name = "btnModificacion";
+			this.btnModificacion.Size = new System.Drawing.Size(75, 23);
+			this.btnModificacion.TabIndex = 11;
+			this.btnModificacion.Text = "Modificacion";
+			this.btnModificacion.UseVisualStyleBackColor = true;
+			this.btnModificacion.Click += new System.EventHandler(this.btnModificacion_Click);
+			// 
+			// btnEliminacion
+			// 
+			this.btnEliminacion.Location = new System.Drawing.Point(331, 345);
+			this.btnEliminacion.Name = "btnEliminacion";
+			this.btnEliminacion.Size = new System.Drawing.Size(75, 23);
+			this.btnEliminacion.TabIndex = 10;
+			this.btnEliminacion.Text = "Eliminacion";
+			this.btnEliminacion.UseVisualStyleBackColor = true;
+			this.btnEliminacion.Click += new System.EventHandler(this.btnEliminacion_Click);
+			// 
+			// btnCancelar
+			// 
+			this.btnCancelar.Location = new System.Drawing.Point(329, 396);
+			this.btnCancelar.Name = "btnCancelar";
+			this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+			this.btnCancelar.TabIndex = 13;
+			this.btnCancelar.Text = "Cancelar";
+			this.btnCancelar.UseVisualStyleBackColor = true;
+			this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+			// 
+			// btnAceptar
+			// 
+			this.btnAceptar.Location = new System.Drawing.Point(218, 396);
+			this.btnAceptar.Name = "btnAceptar";
+			this.btnAceptar.Size = new System.Drawing.Size(75, 23);
+			this.btnAceptar.TabIndex = 12;
+			this.btnAceptar.Text = "Aceptar";
+			this.btnAceptar.UseVisualStyleBackColor = true;
+			this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+			// 
+			// btnCalcular
+			// 
+			this.btnCalcular.Location = new System.Drawing.Point(478, 84);
+			this.btnCalcular.Name = "btnCalcular";
+			this.btnCalcular.Size = new System.Drawing.Size(113, 23);
+			this.btnCalcular.TabIndex = 14;
+			this.btnCalcular.Text = "Calcular";
+			this.btnCalcular.UseVisualStyleBackColor = true;
+			this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+			// 
+			// btnRegistro
+			// 
+			this.btnRegistro.Location = new System.Drawing.Point(478, 45);
+			this.btnRegistro.Name = "btnRegistro";
+			this.btnRegistro.Size = new System.Drawing.Size(113, 23);
+			this.btnRegistro.TabIndex = 15;
+			this.btnRegistro.Text = "Nuevo Registro";
+			this.btnRegistro.UseVisualStyleBackColor = true;
+			this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
+			// 
+			// btnSalir
+			// 
+			this.btnSalir.Location = new System.Drawing.Point(607, 10);
+			this.btnSalir.Name = "btnSalir";
+			this.btnSalir.Size = new System.Drawing.Size(41, 23);
+			this.btnSalir.TabIndex = 16;
+			this.btnSalir.Text = "Salir";
+			this.btnSalir.UseVisualStyleBackColor = true;
+			this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(653, 471);
+			this.Controls.Add(this.btnSalir);
+			this.Controls.Add(this.btnRegistro);
+			this.Controls.Add(this.btnCalcular);
+			this.Controls.Add(this.btnCancelar);
+			this.Controls.Add(this.btnAceptar);
+			this.Controls.Add(this.btnModificacion);
+			this.Controls.Add(this.btnEliminacion);
+			this.Controls.Add(this.btnIngreso);
+			this.Controls.Add(this.btnConsulta);
+			this.Controls.Add(this.txtSalario);
+			this.Controls.Add(this.txtTotal);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.txtEmpleado);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtEmpleado;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox txtDiauno;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtDiaseis;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox txtDiacinco;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox txtDiacuatro;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox txtDiatres;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtDiados;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox txtTotal;
+		private System.Windows.Forms.TextBox txtSalario;
+		private System.Windows.Forms.Button btnConsulta;
+		private System.Windows.Forms.Button btnIngreso;
+		private System.Windows.Forms.Button btnModificacion;
+		private System.Windows.Forms.Button btnEliminacion;
+		private System.Windows.Forms.Button btnCancelar;
+		private System.Windows.Forms.Button btnAceptar;
+		private System.Windows.Forms.Button btnCalcular;
+		private System.Windows.Forms.Button btnRegistro;
+		private System.Windows.Forms.Button btnSalir;
+	}
+}
+
---
+++ 4994880-GRUDINDIVUAL/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+
+namespace _4994880_GRUDINDIVUAL
+{
+	public partial class Form1 : Form
+	{
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//Variable principal para empezar la conexion con la base de datos
+		System.Data.OleDb.OleDbConnection conBD = new System.Data.OleDb.OleDbConnection();
+		String sAction = "";
+
+		public void AbrirConexion()
+		{
+			//Abrir la conexion con la base de datos y mostrar si hay fallo en ello 
+			conBD.ConnectionString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\MINEDUCYT\Desktop\PRACTICA 3\WindowsFomandAccess\Empresa.accdb";
+
+			try
+			{
+				conBD.Open();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show ("Error de conexion" + ex);
+			}
+
+		public void CerrarConexion()
+		{
+			//Se chequea si la conexion esta habilitada y luego la cierra
+			if (conBD.State == ConnectionState.Open)
+			{
+				conBD.Close();
+			}
+		}
+
+		private void desactivarBotones()
+		{
+			btnConsulta.Enabled = false;
+			btnIngreso.Enabled = false;
+			btnEliminacion.Enabled = false;
+			btnModificacion.Enabled = false;
+
+			txtDiauno.Enabled = false;
+			txtDiados.Enabled = false;
+			txtDiatres.Enabled = false;
+			txtDiacuatro.Enabled = false;
+			txtDiacinco.Enabled = false;
+			txtDiaseis.Enabled = false;
+
+			btnAceptar.Enabled = true;
+			btnCancelar.Enabled = true;
+		}
+
+		private void activarBotones()
+		{
+			btnConsulta.Enabled = true;
+			btnIngreso.Enabled = true;
+			btnEliminacion.Enabled = true;
+			btnModificacion.Enabled = true;
+			btnAceptar.Enabled = false;
+			btnCancelar.Enabled = false;
+
+			// Se cancela la accion a realizar
+			sAction = "";
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			AbrirConexion();
+		}
+
+
+		private void btnCalcular_Click(object sender, EventArgs e)
+		{
+
+			//Calcular el total de horas laboradas
+			int HorasT = (int.Parse(txtDiauno.Text) + int.Parse(txtDiados.Text) + int.Parse(txtDiatres.Text) + int.Parse(txtDiacuatro.Text)
+				+ int.Parse(txtDiacinco.Text) + int.Parse(txtDiaseis.Text));
+
+			//Calcular el salario
+			double Cantidadsalario = 1.50;
+			double Salario = Cantidadsalario * HorasT;
+
+			//Mostrando resultados
+
+			txtTotal.Text = HorasT.ToString();
+			txtSalario.Text = Salario.ToString();
+		}
+
+		private void btnRegistro_Click(object sender, EventArgs e)
+		{
+			txtEmpleado.Text = "";
+			txtDiauno.Text = "";
+			txtDiados.Text = "";
+			txtDiatres.Text = "";
+			txtDiacuatro.Text = "";
+			txtDiacinco.Text = "";
+			txtDiaseis.Text = "";
+			txtTotal.Text = "";
+			txtSalario.Text = "";
+		}
+
+		private void btnSalir_Click(object sender, EventArgs e)
+		{
+			//S edebe cerrar la conexion
+			CerrarConexion();
+
+			//Se cierra el programa
+			this.Close();
+		}
+
+		private void btnConsulta_Click(object sender, EventArgs e)
+		{
+			//Se indica la accion a realizar
+			sAction = "Consulta";
+			//Desactivamos los botones
+			desactivarBotones();
+		}
+
+		private void btnIngreso_Click(object sender, EventArgs e)
+		{
+			//Se indica la accion a realizar
+			sAction = "Ingreso";
+			//Desactivamos los botones
+			desactivarBotones();
+		}
+
+		private void btnEliminacion_Click(object sender, EventArgs e)
+		{
+			//Accion que se realizara
+			sAction = "Eliminacion";
+			//Desactivar botones
+			desactivarBotones();
+		}
+
+		private void btnModificacion_Click(object sender, EventArgs e)
+		{
+			//Accion que se realizara
+			sAction = "Modificacion";
+			//Fuera botones
+			desactivarBotones();
+		}
+
+		private void btnCancelar_Click(object sender, EventArgs e)
+		{
+			activarBotones();
+		}
+
+		private void btnAceptar_Click(object sender, EventArgs e)
+		{
+
+			//----------------------------------CONSULTA-------------------------------
+			if (sAction == "Consulta")
+			{
+				try
+				{
+					//Se realiza la consulta
+					String sConsulta = "SELECT *  "
+						+ "FROM Empleados  "
+						+ "WHERE Empleado = '" + txtEmpleado.Text + "'";
+
+					OleDbCommand oleComando = new OleDbCommand(sConsulta, conBD);
+					OleDbDataReader oleReader = oleComando.ExecuteReader();
+
+					//Se lee el resultado para ver si existe alguno
+					if (oleReader.Read())
+					{
+						//SE muestra los resultados
+						txtTotal.Text = Convert.ToString(oleReader["Horas Trabajadas"]);
+						txtSalario.Text = Convert.ToString(oleReader["Salario"]);
+					}
+					else
+					{
+						//Si no existe dar un mensaje
+						MessageBox.Show("El empleado no esta registrado");
+					}
+				}
+				catch (Exception ex)
+				{
+					//Mensaje de error
+					MessageBox.Show("Valor ingresado invaliudo" + ex);
+
+				}
+			}
+
+			//----------------------------------CODIGO DE INGRESO-------------------------
+
+			else
+			{
+				if (sAction == "Ingreso")
+				{
+					try
+					{
+						//Se inserta el codigo 
+						String sConsulta = "INSERT INTO Empleados "
+							+ " VALUES ('" + txtEmpleado.Text + "'," + txtTotal.Text + "," + txtSalario.Text + ")";
+
+						//Se escribe el codigo para hacer efectiva la consulta y quese ejute
+						OleDbCommand oleComando = new OleDbCommand();
+						oleComando.CommandType = CommandType.Text;
+						oleComando.CommandText = sConsulta;
+						oleComando.Connection = conBD;
+
+						// Se ejecuta
+						oleComando.ExecuteNonQuery();
+
+						//Limpiamos los textboxs
+
+						txtEmpleado.Text = "";
+						txtTotal.Text = "";
+						txtSalario.Text = "";
+
+
+					}
+					catch (Exception ex)
+					{
+						//MOstra mensaje de error si hay pronlemas al ingresar
+						MessageBox.Show("No es posible ingresar datos en la base" + ex);
+					}
+				}
+				else
+				{
+					//----------------------------------CODIGO DE ELIMINACION-----------------------------------
+					if (sAction == "Eliminacion")
+					{
+						try
+						{
+							//Se escribe la consulta
+							String sConsulta = "DELETE FROM Empleados "
+												 + " WHERE Empleado = '" + txtEmpleado.Text + "'";
+
+							OleDbCommand oleComando = new OleDbCommand();
+							oleComando.CommandType = CommandType.Text;
+							oleComando.CommandText = sConsulta;
+							oleComando.Connection = conBD;
+
+							//Se ejcuta la conulta
+							oleComando.ExecuteNonQuery();
+
+							//Limpiamos los textBox
+
+							txtEmpleado.Text = "";
+							txtTotal.Text = "";
+							txtSalario.Text = "";
+						}
+						catch (Exception ex)
+						{
+							//MOstrar mensaje si hay fallo
+							MessageBox.Show("No se pudo eliminar el registro" + ex);
+						}
+					}
+					else
+					{
+							try
+							{
+								// Se escribe la sintaxis
+								String sConsulta = "UPDATE Empleados "
+									+ " SET Salario = " + txtSalario.Text + " , "
+									+ " [Horas Trabajadas] = " + txtTotal.Text + " "
+									+ " WHERE Empleado = '" + txtEmpleado.Text+"' ";
+
+							OleDbCommand oleComando = new OleDbCommand();
+								oleComando.CommandType = CommandType.Text;
+								oleComando.CommandText = sConsulta;
+								oleComando.Connection = conBD;
+
+								//Se ejcuta la conulta
+								oleComando.ExecuteNonQuery();
+
+								//Limpiamos los textBox
+
+								txtEmpleado.Text = "";
+								txtTotal.Text = "";
+								txtSalario.Text = "";
+							}
+							catch (Exception ex)
+							{
+								MessageBox.Show("No se pudo modificar el registro" + ex);
+							}
+						
+					}
+				}
+			}
+			//Se termina la accion
+			sAction = "";
+
+			activarBotones();
+		}
+	}
+}
+
+
